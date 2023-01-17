fe1af4e298a4124c29ef4099bf6af78f428753e6
CubeTures <91618801+CubeTures@users.noreply.github.com>
2023-01-17 09:01:17 -0600

    Update shadows.js

--- Include/shadows.js
+++ Include/shadows.js
@@ -11,27 +11,16 @@ function spaced() {
     let divs = document.getElementsByClassName('spaced');
     for(let x = 0; x < divs.length; x++) {
         let div = divs.item(x);
+        console.log(div.innerHTML);
         let len = parseInt(div.innerHTML);
 
         let inner = "";
         for(let i = 0; i < len; i++) {
             inner += '\n';
         }
-        div.innerHTML = inner + '\n<p>Test</p>';
+        div.innerHTML = inner;
 
         div.style = "white-space: break-spaces";
-
-        // let bgTop = "linear-gradient(canvas, ";
-        // let bgBot = "linear-gradient(";
-        // for(let i = 0; i < len / shadowDensity; i++) {
-        //     bgTop += alpha + ", ";
-        //     bgBot += alpha + ", ";
-        // }
-        // bgTop = bgTop.substring(0, bgTop.length - 2) + ")";
-        // bgBot = bgBot.substring(0, bgTop.length - 2) + "canvas)";
-
-        // let bg = bgTop + ", " + bgBot;
-        // div.style.background = bg;
     }
 }
 
