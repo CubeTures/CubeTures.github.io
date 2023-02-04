a3302859492256e2326f77f68961f80cda20627b
CubeTures <shadbowne@gmail.com>
2023-02-04 09:48:13 -0600

    all but active outline

--- Include/new_nav.js
+++ Include/new_nav.js
@@ -1,37 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
     //Menu on Hover
-    document.body.addEventListener('mouseenter', (e) => { onHover(e); });
-    document.body.addEventListener('mouseleave', (e) => { onHover(e); });
+    $('body').on('mouseenter mouseleave', '.nav-item', function (e) {
+        //768
+        if($(window).width() > 768) {
+            var _d = e.target.closest('.nav-item');
+            _d.classList.add('show');
+            setTimeout(() => {
+                if(_d.matches(':hover')) {
+                    _d.classList.add('show');
+                }
+                else {
+                    _d.classList.remove('show');
+                }
+            }, 1);
+        }
+    });
 
     //Light / Dark Theme
-    document.getElementById('switch').addEventListener('click', () => {
+    document.querySelector('.logo').addEventListener('click', () => {
         let body = document.body;
-        let toggle = document.getElementById('switch');
-        let light = document.getElementById('logo-light');
-        let dark = document.getElementById('logo-dark');
 
         if(body.classList.contains('dark')) {
             body.classList.remove('dark');
-            toggle.classList.remove('switched');
         }
         else {
             body.classList.add('dark');
-            toggle.classList.add('switched')
         }
     });
 });
-
-function onHover(e) {
-    $('body').on('mouseenter mouseleave', '.nav-item', function (e) {
-        var _d = e.target.closest('.nav-item');
-        _d.classList.add('show');
-        setTimeout(() => {
-            if(_d.matches(':hover')) {
-                _d.classList.add('show');
-            }
-            else {
-                _d.classList.remove('show');
-            }
-        }, 1);
-    });
-}
