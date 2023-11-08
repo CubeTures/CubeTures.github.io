51a346c8beaaae7ae79673ec0c0ec2bba45aa73b
CubeTures <shadbowne@gmail.com>
2023-11-07 23:45:57 -0600

    cleaned files

--- Include/MealMatch/FirebaseLogin.js
+++ Include/MealMatch/FirebaseLogin.js
@@ -1,50 +1,5 @@
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-auth.js";
-import { getDatabase, ref, onValue, set } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-database.js";
-let userData;
-let unsubscribeOnValue;
-
-function getReference(uid) {
-    const db = getDatabase();
-    const path = `users/${uid}`;
-    return ref(db, path);
-}
-function setUserData(snapshot, callback) {
-    userData = snapshot.val();
-    unsubscribeOnValue();
-    callback();
-}
-function updateUserData(reference, callback) {
-    unsubscribeOnValue = onValue(reference, 
-        (snapshot) => setUserData(snapshot, callback));
-}
-
-function updateDisplay() {
-    const elem = document.getElementById("data");
-    elem.innerHTML = JSON.stringify(userData);
-}
-
-function getNewUserData(displayName) {
-    return {
-        "username": displayName,
-        "friends": [],
-        "session": {},
-        "requests": []
-    }
-}
-function setNewUserData(reference, displayName) {
-    let newUserData = getNewUserData(displayName);
-    set(reference, newUserData);
-    userData = newUserData;
-}
-function setupDatabase(user) {
-    const reference = getReference(user.uid);
-    updateUserData(reference, () => {
-        if(userData == null) {
-            setNewUserData(reference, user.displayName);
-        }
-        updateDisplay();
-    });
-}
+import { setupDatabase } from "./FirebaseDatabase";
 
 class FakeUser {
     constructor(uid, displayName) {
@@ -53,7 +8,6 @@ class FakeUser {
     }
 }
 function debugLogin() {
-    //"Y8Zw66hR3lhZIp0p213npkBAVq52"
     let fakeUser = new FakeUser("fakeID", "fakeDisplayName");
     setupDatabase(fakeUser);
 }
--- Include/MealMatch/InitializeFirebase.js
+++ Include/MealMatch/InitializeFirebase.js
@@ -1,9 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBegtSVJn6lWy_0quk2oELyy0woSRmCqN8",
   authDomain: "mealmatch-40661.firebaseapp.com",
@@ -14,6 +10,4 @@ const firebaseConfig = {
   appId: "1:691358567925:web:3411cdb0ef590dc93324bc"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export { app }
