2e50735b961a774034740239ff948a41c080aa65
CubeTures <shadbowne@gmail.com>
2023-11-07 23:36:18 -0600

    basic firebase

--- Include/MealMatch/FirebaseLogin.js
+++ Include/MealMatch/FirebaseLogin.js
@@ -1,21 +1,70 @@
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-auth.js";
-import {  } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-auth.js";
-const auth = getAuth()
+import { getDatabase, ref, onValue, set } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-database.js";
+let userData;
+let unsubscribeOnValue;
 
-function loginComplete(user) {
-    console.log(user.uid);
+function getReference(uid) {
+    const db = getDatabase();
+    const path = `users/${uid}`;
+    return ref(db, path);
+}
+function setUserData(snapshot, callback) {
+    userData = snapshot.val();
+    unsubscribeOnValue();
+    callback();
+}
+function updateUserData(reference, callback) {
+    unsubscribeOnValue = onValue(reference, 
+        (snapshot) => setUserData(snapshot, callback));
 }
 
-function debugLogin() {
+function updateDisplay() {
+    const elem = document.getElementById("data");
+    elem.innerHTML = JSON.stringify(userData);
+}
 
+function getNewUserData(displayName) {
+    return {
+        "username": displayName,
+        "friends": [],
+        "session": {},
+        "requests": []
+    }
+}
+function setNewUserData(reference, displayName) {
+    let newUserData = getNewUserData(displayName);
+    set(reference, newUserData);
+    userData = newUserData;
+}
+function setupDatabase(user) {
+    const reference = getReference(user.uid);
+    updateUserData(reference, () => {
+        if(userData == null) {
+            setNewUserData(reference, user.displayName);
+        }
+        updateDisplay();
+    });
+}
+
+class FakeUser {
+    constructor(uid, displayName) {
+        this.uid = uid;
+        this.displayName = displayName;
+    }
+}
+function debugLogin() {
+    //"Y8Zw66hR3lhZIp0p213npkBAVq52"
+    let fakeUser = new FakeUser("fakeID", "fakeDisplayName");
+    setupDatabase(fakeUser);
 }
 
 function login() {
+    const auth = getAuth()
     const provider = new GoogleAuthProvider();
     
     signInWithPopup(auth, provider)
         .then((result) => {
-            loginComplete(result.user);
+            setupDatabase(result.user);
         })
         .catch((error) => {
             console.error("Login error:", error);
@@ -26,6 +75,5 @@ function onContentLoad() {
     let loginButton = document.getElementById("login");
     loginButton.addEventListener("click", login);
 }
-
 
 document.addEventListener("DOMContentLoaded", onContentLoad);
