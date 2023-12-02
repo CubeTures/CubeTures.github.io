0aad0a481c5af5abaae9134a088509f2067a97c2
CubeTures <shadbowne@gmail.com>
2023-12-01 18:55:52 -0600

    new match html almost complete

---
+++ Include/MealMatch/google-api.js
@@ -0,0 +1,35 @@
+const apiKey = 'AIzaSyBegtSVJn6lWy_0quk2oELyy0woSRmCqN8';
+const apiUrl = 'https://places.googleapis.com/v1/places:searchNearby';
+
+const header = {
+    "includedTypes": ["restaurant"],
+    "maxResultCount": 20,
+    "locationRestriction": {
+      "circle": {
+        "center": {
+          "latitude": 37.7937,
+          "longitude": -122.3965},
+        "radius": 500.0
+      }
+    }
+}
+
+function createRequest() {
+    fetch(apiUrl, {
+        method: 'POST',
+        body: JSON.stringify(header)
+    }).then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        console.log(JSON.stringify(response.json, null, 2));
+        return response.json();
+    }).then(data => {
+        console.log(data);
+    }).catch(error => {
+        console.error('Error fetching data:', error);
+    });
+}
+
+//createRequest();
--- Include/MealMatch/new-match.js
+++ Include/MealMatch/new-match.js
@@ -2,6 +2,45 @@ let latitudeInput, longitudeInput;
 let radiusInput;
 let numResultsInput;
 
+function onDocumentLoad() {
+    setVariables();
+    setOnClicks();
+}
+function setVariables() {
+    latitudeInput = document.getElementById("latitude");
+    longitudeInput = document.getElementById("longitude");
+    radiusInput = document.getElementById("radius");
+    numResultsInput = document.getElementById("numresults");
+}
+function setOnClicks() {
+    document.getElementById("mylocation")
+        .addEventListener("click", setLocation);
+}
+
+function setLocation() {
+    getCurrentLocation((position) => {
+        latitudeInput.value = position.coords.latitude;
+        longitudeInput.value = position.coords.longitude;
+    });
+}
+function getCurrentLocation(callback) {
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(callback);
+    }
+    else {
+        console.log("Geolocation error");
+    }
+}
+
+function parseInputData() {
+    checkForData();
+    let inputData = {}
+    inputData["ll"] = parseLocation();
+    inputData["radius"] = parseRadius();
+    inputData["limit"] = parseNumResults();
+
+    return inputData;
+}
 function parseLocation() {
     return `${latitudeInput.value},${longitudeInput.value}`
 }
@@ -15,31 +54,10 @@ function parseNumResults() {
     return numResultsInput.value;
 }
 
-function getCurrentLocation(callback) {
-    if(navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(callback);
-    }
-    else {
-        console.log("Geolocation error");
-    }
-}
-function setLocation() {
-    getCurrentLocation((position) => {
-        latitudeInput.value = position.coords.latitude;
-        longitudeInput.value = position.coords.longitude;
-    });
+function checkForData() {
+    //check to see if all the data is filled, otherwise give errors using bootstrap
 }
 
-function setClick(id, callback) {
-    document.getElementById(id)
-        .addEventListener("click", callback);
-}
-document.addEventListener("DOMContentLoaded", () => {
-    latitudeInput = document.getElementById("latitude");
-    longitudeInput = document.getElementById("longitude");
-    radiusInput = document.getElementById("radius");
-    numResultsInput = document.getElementById("numresults");
-    setClick("mylocation", setLocation);
-});
 
-export { parseLocation, parseRadius, parseNumResults }
+document.addEventListener("DOMContentLoaded", onDocumentLoad);
+export { parseInputData }
